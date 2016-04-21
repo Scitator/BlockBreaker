5c939cb1fc59a822f0133b46c99e27ea00fee360
Serge Kolesnikov <serge.s.kolesnikov@gmail.com>
2016-04-21 16:17:42 +0300

    initial commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name) {
+		//Debug.Log ("New level load: " + name);
+		Brick.breakableCount = 0;
+		Application.LoadLevel (name);
+	}
+
+	public void LoadNextLevel() {
+		//Debug.Log ("New level load: " + name);
+		Brick.breakableCount = 0;
+		Application.LoadLevel (Application.loadedLevel + 1);
+	}
+
+	public void Quit() {
+		//Debug.Log ("Quit");
+		Application.Quit ();
+	}
+
+	public void BrickDestroyed() {
+		if (Brick.breakableCount <= 0) {
+			LoadNextLevel();
+		}
+	}
+}
